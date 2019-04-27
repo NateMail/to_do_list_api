9edb64f6997b886c8b8d5beef09405f905b3ba1f
nathanmailhiot <nathanjm12@outlook.com>
2019-04-27 16:45:28 -0400

    Fix spelling error

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,13 +43,13 @@ class ItemsController < ProtectedController
 
   private
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_item
-      @item = current_user.items.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_item
+    @item = current_user.items.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def item_params
-      params.require(:item).permit(:title, :description)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def item_params
+    params.require(:item).permit(:title, :description)
+  end
 end
